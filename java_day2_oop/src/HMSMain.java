e5607eefb60b3c7ab19ea073787f6615b6db8077
nochanghwan <changhwanno@gamil.com>
2017-07-17 14:50:48 +0900

    second

--- java_day2_oop/src/HMSMain.java
+++ java_day2_oop/src/HMSMain.java
@@ -1,3 +1,4 @@
+import model.vo.PersonVO;
 import model.vo.sub.ManagerVO;
 import model.vo.sub.StudentVO;
 import model.vo.sub.TeacherVO;
@@ -6,14 +7,39 @@ public class HMSMain {
 
 	public static void main(String[] args) {
 	
-		StudentVO stu = new StudentVO("학생","노창환",25,"인천","2012");
-		System.out.println(stu.printStu());
-	
-		TeacherVO tea = new TeacherVO("학생","임정섭",25,"인천","자바");
-		System.out.println(tea.printTea());
+		PersonVO stu = new StudentVO("학생","노창환",25,"인천","2012");
+		System.out.println(((StudentVO)stu).printStu());
+		// 캐스팅시킬수있다.
+		PersonVO tea = new TeacherVO("학생","임정섭",25,"인천","자바");
+		System.out.println(((TeacherVO)tea).printTea());
 
-		ManagerVO emp = new ManagerVO("학생","방가은",25,"인천","매니저");
-		System.out.println(emp.printEmp());
+		PersonVO emp = new ManagerVO("학생","방가은",25,"인천","매니저");
+		System.out.println( ((ManagerVO)emp).printEmp());
+		
+		/////////////////////////////////////////
+		
+		PersonVO [] perAry = new PersonVO[10];
+		int idx = 0;
+		perAry[idx++] = stu;
+		perAry[idx++] = tea;
+		perAry[idx++] = emp;
+		
+		System.out.println("출력");
+		for(int i=0;i<perAry.length;i++){
+			PersonVO person = perAry[i];
+			if(person == null){
+				break;
+			}
+			System.out.println(person.printPer());
+		}
+		
+		System.out.println("test");
 
-	}
+		for(int i=0;i<perAry.length;i++){
+			System.out.println(perAry[i].printPer());
+		}
+		
+		
+		
+	}	
 }
---
+++ java_day2_oop/src/PolyMain.java
@@ -0,0 +1,9 @@
+
+public class PolyMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
